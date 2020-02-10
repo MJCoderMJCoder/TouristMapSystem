ff47375dda53f31e13d0f31f9fa836b95b3e3ded
纸纷飞 <598157378@qq.com>
2020-02-10 15:57:41 +0800

    因为润世华需求在润世华MS时进行了相应更改

--- src/main/java/com/lzf/web/ControlUserAssociated.java
+++ src/main/java/com/lzf/web/ControlUserAssociated.java
@@ -1,12 +1,12 @@
 /**
- * 
+ *
  */
 package com.lzf.web;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.junit.Test;
+//import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,34 +24,34 @@ import com.lzf.service.impl.ServiceUserAssociated;
 @RequestMapping("userassociated")
 public class ControlUserAssociated {
 
-	@Resource
-	private IServiceUserAssociated serviceUserAssociated;
+    @Resource
+    private IServiceUserAssociated serviceUserAssociated;
 
-	/**
-	 * 
-	 */
-	public ControlUserAssociated() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     *
+     */
+    public ControlUserAssociated() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * 添加游客关联信息
-	 * 
-	 * @param request
-	 * @return
-	 */
-	@RequestMapping(value = "insert", method = RequestMethod.POST)
-	@ResponseBody
-	private Object insert(HttpServletRequest request) {
-		int userselfId = Integer.parseInt(request.getParameter("userselfId"));
-		int userRelationshipId = Integer.parseInt(request.getParameter("userRelationshipId"));
-		int result = serviceUserAssociated.insert(new UserAssociated(0, null, userselfId, userRelationshipId, null));
-		DtoPackaging dtoPackaging = null;
-		if (result <= 0) {
-			dtoPackaging = new DtoPackaging(false, "游客关联信息录入失败", null);
-		} else {
-			dtoPackaging = new DtoPackaging(true, "游客关联信息录入成功", null);
-		}
-		return dtoPackaging;
-	}
+    /**
+     * 添加游客关联信息
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "insert", method = RequestMethod.POST)
+    @ResponseBody
+    private Object insert(HttpServletRequest request) {
+        int userselfId = Integer.parseInt(request.getParameter("userselfId"));
+        int userRelationshipId = Integer.parseInt(request.getParameter("userRelationshipId"));
+        int result = serviceUserAssociated.insert(new UserAssociated(0, null, userselfId, userRelationshipId, null));
+        DtoPackaging dtoPackaging = null;
+        if (result <= 0) {
+            dtoPackaging = new DtoPackaging(false, "游客关联信息录入失败", null);
+        } else {
+            dtoPackaging = new DtoPackaging(true, "游客关联信息录入成功", null);
+        }
+        return dtoPackaging;
+    }
 }
